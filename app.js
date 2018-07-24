7215f9135e3bb47105aa79c3ea2c35bd498fa529
Philip Jägenstedt <philip@foolip.org>
2018-07-24 23:04:39 +0000

    Commit experimentation/exploration code
    Result: https://github.com/foolip/web-platform-tests/pull/1/checks

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const PORT = 8080;
 
-const PORT = 8080
+const app = express();
+app.use(express.json({
+    verify: (req, res, buf, encoding) => {
+	const signature = req.header('x-hub-signature')
+	console.log(`TODO: verify signature ${signature}`)
+	if (signature != 'sha1=30816a3aa38e10f66819a3c84868db9cc87cd2a2')
+	    throw new Error(`Signature mismatch`);
+    }
+}));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/callback', (req, res) => {
-    console.log(req)
-    res.send('callback handler')
-})
+    res.send('callback handler');
+    console.log(req.body);
+});
 
 app.post('/webhook', (req, res) => {
-    console.log(req)
-    res.send('webhook handler')
+    const deliveryId = req.header('x-github-delivery');
+    console.log(`X-GitHub-Delivery: ${deliveryId}`);
+    const event = req.header('x-github-event');
+    if (event !== 'check_suite')
+	throw new Error(`Unsupported event: ${event}`);
+    res.send('OK');
 })
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ auth.js
@@ -0,0 +1,61 @@
+const octokit = require('@octokit/rest')();
+const jwt = require('jsonwebtoken');
+
+// getToken() does two things:
+// https://developer.github.com/apps/building-github-apps/authenticating-with-github-apps/#authenticating-as-a-github-app
+// https://developer.github.com/v3/apps/#create-a-new-installation-token
+//
+// TODO: "Installation tokens expire one hour from the time you create them."
+async function getToken() {
+    const cert = process.env.APP_PEM;
+    const appId = process.env.APP_ID;
+
+    const appToken = jwt.sign({}, cert, {
+	algorithm: 'RS256',
+	expiresIn: 600,
+	issuer: appId,
+    });
+
+    octokit.authenticate({
+	type: 'app',
+	token: appToken,
+    });
+
+    const result = await octokit.apps.createInstallationToken({installation_id: 255453});
+    //console.log(result);
+    return result.data.token
+}
+
+async function main() {
+    const token = await getToken();
+
+    octokit.authenticate({
+	type: 'token',
+	token: token,
+    });
+
+    const result = await octokit.checks.create({
+	owner: 'foolip',
+	repo: 'web-platform-tests',
+	name: 'Test check',
+	head_branch: 'test-branch',
+	head_sha: 'ea3335dc1719ab30eab8f8bbd6d40da48a5e9a1e',
+	details_url: 'https://app.foolip.org/stuff',
+	conclusion: 'neutral',
+	completed_at: new Date().toISOString(),
+	output: {
+	    title: 'Another check title',
+	    summary: 'Does markdown *work*?',
+	    text: 'More words\nAnd lines\n[links work](https://example.com)',
+	},
+	actions: [{
+	    label: 'Ignore this',
+	    description: 'More words',
+	    identifier: 'TODO',
+	}],
+    });
+
+    console.log(result);
+}
+
+main();

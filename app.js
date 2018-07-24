c452206f54de62706b98549928f7dd945a10f04c
Philip Jägenstedt <philip@foolip.org>
2018-07-24 13:51:26 +0000

    Handle GET /callback and POST /webhook

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 
+const PORT = 8080
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(8080, () => console.log('Example app listening on port 3000!'))
+app.get('/callback', (req, res) => {
+    console.log(req)
+    res.send('callback handler')
+})
+
+app.post('/webhook', (req, res) => {
+    console.log(req)
+    res.send('webhook handler')
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))

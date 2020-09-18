0a05f2ecd2626af413aa0845baf41aba33778ab6
Philip Jägenstedt <philip@foolip.org>
2020-09-18 08:56:44 +0200

    Use bunyan with middleware for logging (instead of winston)
    Because we want to be like wpt-pr-bot when we grow up:
    https://github.com/web-platform-tests/wpt-pr-bot/pull/159
    
    Parts of this are still very unsatisfactory, passing around the logger
    isn't great, and using `req.log` after the request has ended will
    probably lead to somethings strange.

--- app.js
+++ app.js
@@ -12,75 +12,102 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const bunyan = require('bunyan');
+const cloudBunyan = require('@google-cloud/logging-bunyan');
 const express = require('express');
-const checks = require('./lib/checks.js');
-const logger = require('./lib/logger');
 
-const app = express();
-app.use(express.json({
-  verify: (req, res, buf, encoding) => {
-    const secret = process.env.APP_WEBHOOK_SECRET;
-    if (!secret) {
+const checks = require('./lib/checks.js');
+
+function createExpressApp(logMiddleware) {
+  const app = express();
+
+  app.use(logMiddleware);
+
+  app.use(express.json({
+    verify: (req, res, buf, encoding) => {
+      const secret = process.env.APP_WEBHOOK_SECRET;
+      if (!secret) {
+        return;
+      }
+      const signature = req.header('x-hub-signature');
+      req.log.debug(`TODO: verify signature ${signature}`);
+      if (signature != 'sha1=30816a3aa38e10f66819a3c84868db9cc87cd2a2') {
+        throw new Error(`Signature mismatch`);
+      }
+    },
+  }));
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.post('/webhook', (req, res) => {
+    req.log.debug('/webhook invoked');
+
+    const deliveryId = req.header('x-github-delivery');
+    req.log.debug(`X-GitHub-Delivery: ${deliveryId}`);
+    const event = req.header('x-github-event');
+
+    const payload = req.body;
+
+    req.log.debug('/webhook payload', payload);
+
+    if (event !== 'check_suite') {
+      req.log.info(`ignoring ${event} event`);
+      res.end();
       return;
     }
-    const signature = req.header('x-hub-signature');
-    logger.debug(`TODO: verify signature ${signature}`);
-    if (signature != 'sha1=30816a3aa38e10f66819a3c84868db9cc87cd2a2') {
-      throw new Error(`Signature mismatch`);
+
+    if (payload.action !== 'requested') {
+      req.log.info(`ignoring check_suite action ${payload.action}`);
+      res.end();
+      return;
     }
-  },
-}));
 
-app.get('/', (req, res) => {
-  logger.debug('GET /');
-  res.send('Hello World!');
-});
-
-app.get('/callback', (req, res) => {
-  res.send('TODO');
-});
-
-app.post('/webhook', (req, res) => {
-  logger.debug('/webhook invoked');
-
-  const deliveryId = req.header('x-github-delivery');
-  logger.debug(`X-GitHub-Delivery: ${deliveryId}`);
-  const event = req.header('x-github-event');
-
-  const payload = req.body;
-
-  if (event == 'check_run') {
-    logger.debug('ignoring check_run event');
+    const data = {
+      app_id: payload.check_suite.app.id,
+      installation_id: payload.installation.id,
+      owner: payload.repository.owner.login,
+      repo: payload.repository.name,
+      head_branch: payload.check_suite.head_branch,
+      head_sha: payload.check_suite.head_sha,
+    };
+    req.log.info('/webhook check_suite extracted data:', data);
     res.end();
-    return;
+
+    // Do the work later.
+    setTimeout(() => checks.create(data, req.log));
+  });
+
+  return app;
+}
+
+async function main() {
+  let logger;
+  let mw;
+  if (process.env.GOOGLE_CLOUD_PROJECT) {
+    // On AppEngine, use @google-cloud/logging-bunyan middleware.
+    ({logger, mw} = await cloudBunyan.express.middleware());
+  } else {
+    // Locally, log to stdout and make `req.log` available.
+    logger = bunyan.createLogger({
+      name: 'spec-test-bot',
+      streams: [{stream: process.stdout, level: 'debug'}],
+    });
+    mw = (req, res, next) => {
+      req.log = logger;
+      next();
+    };
   }
 
-  if (event !== 'check_suite') {
-    throw new Error(`Unexpected event: ${event}`);
-  }
+  const app = createExpressApp(mw);
 
-  logger.debug(`check_suite action: ${payload.action}`);
-  if (payload.action != 'requested') {
-    res.end();
-    return;
-  }
+  const port = process.env.PORT || 8080;
+  app.listen(port, () => {
+    logger.info(`Listening on port ${port}`);
+  });
+}
 
-  const data = {
-    app_id: payload.check_suite.app.id,
-    installation_id: payload.installation.id,
-    owner: payload.repository.owner.login,
-    repo: payload.repository.name,
-    head_branch: payload.check_suite.head_branch,
-    head_sha: payload.check_suite.head_sha,
-  };
-  logger.debug('/webhook check_suite extracted data:', data);
-  res.end();
-
-  // Do the work later.
-  setTimeout(() => checks.create(data));
-});
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  logger.info(`Listening on port ${port}`);
-});
+if (require.main === module) {
+  main();
+}

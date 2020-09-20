cb6668aa113be6be78d45652fb3af2ffe31d8122
Philip Jägenstedt <philip@foolip.org>
2020-09-21 00:02:56 +0200

    Get rid of @octokit/webhooks again
    To make it easier to reuse `jsonVerifier` for task queue payloads.

--- app.js
+++ app.js
@@ -12,14 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const {createHmac} = require('crypto');
 const bunyan = require('bunyan');
 const express = require('express');
 const {LoggingBunyan} = require('@google-cloud/logging-bunyan');
-const {Webhooks} = require('@octokit/webhooks');
 
 const {getOctokit} = require('./lib/octokit.js');
 const checks = require('./lib/checks.js');
 const secrets = require('./secrets.json');
+
+function sign(data, secret) {
+  const digest = createHmac('sha1', secret).update(data).digest('hex');
+  return `sha1=${digest}`;
+}
+
+function jsonVerifier(header, secret, logger) {
+  return express.json({
+    verify: (req, res, buf) => {
+      const actual = req.get(header);
+      if (typeof actual !== 'string') {
+        throw new Error(`no ${header} header`);
+      }
+      const expected = sign(buf, secret);
+      if (actual !== expected) {
+        logger.error({actual, expected}, 'signature mismatch');
+        throw new Error('signature mismatch');
+      }
+    },
+  });
+}
 
 function getOctokitFor(payload) {
   return getOctokit({
@@ -32,97 +53,95 @@ function getOctokitFor(payload) {
 function createExpressApp(logger) {
   const app = express();
 
+  app.use(jsonVerifier('x-hub-signature',
+      secrets.github.webhook_secret, logger));
+
   app.get('/', (req, res) => {
     res.send('Hello World!');
   });
 
-  const webhooks = new Webhooks({
-    secret: secrets.github.webhook_secret,
-    path: '/webhook',
-  });
+  app.post('/api/webhook', (req, res, next) => {
+    const event = req.get('x-github-event');
+    const delivery = req.get('x-github-delivery');
+    const payload = req.body;
+    logger.info({event, delivery, payload}, 'webhook received');
 
-  app.use(webhooks.middleware);
+    switch (event) {
+      case 'check_run': {
+        if (payload.action === 'rerequested') {
+          logger.warn('restarting check run not implemented');
+        } else if (payload.action === 'requested_action') {
+          logger.warn('handling user actions not implemented');
+        }
+        res.end();
+        break;
+      }
+      case 'check_suite': {
+        if (payload.action !== 'requested' &&
+            payload.action !== 'rerequested') {
+          return;
+        }
 
-  webhooks.on('check_run', (event) => {
-    logger.debug({event}, 'check_run event');
+        const data = {
+          owner: payload.repository.owner.login,
+          repo: payload.repository.name,
+          head_branch: payload.check_suite.head_branch,
+          head_sha: payload.check_suite.head_sha,
+        };
 
-    const {payload} = event;
+        // Do the work async.
+        logger.info({data}, 'scheduling check creation');
+        setTimeout(() => {
+          const octokit = getOctokitFor(payload);
+          checks.create(octokit, data, logger);
+        });
+        res.end();
+        break;
+      }
+      case 'commit_comment':
+      case 'issue_comment':
+      case 'pull_request_review_comment': {
+        if (payload.action !== 'created' && payload.action !== 'edited') {
+          return;
+        }
 
-    if (payload.action === 'rerequested') {
-      logger.warn('restarting check run not implemented');
-    } else if (payload.action === 'requested_action') {
-      logger.warn('handling user actions not implemented');
+        // To avoid a comment loop in the event of a mishap, do not react to any
+        // bot comments, importantly including our own ones.
+        if (payload.comment.user.type === 'Bot') {
+          const name = payload.comment.user.login;
+          logger.info(`ignoring bot comment from ${name}`);
+          return;
+        }
+
+        // Do nothing if this is not directed at @spec-test-bot.
+        // TODO: Avoid reacting to '@spec-test-botanist'
+        const atMentioned = payload.comment.body.includes('@spec-test-bot');
+        if (!atMentioned) {
+          return;
+        }
+
+        if (event !== 'issue_comment') {
+          logger.warn('comment reactions not implemented');
+          return;
+        }
+
+        const octokit = getOctokitFor(payload);
+
+        octokit.reactions.createForIssueComment({
+          owner: payload.repository.owner.login,
+          repo: payload.repository.name,
+          comment_id: payload.comment.id,
+          content: '+1',
+        }).then(() => {
+          res.end();
+        }, next);
+        break;
+      }
+      default:
+        logger.info(`$${event} webhook event ignored`);
+        res.end();
+        break;
     }
-  });
-
-  webhooks.on('check_suite', (event) => {
-    logger.debug({event}, 'check_suite event');
-
-    const {payload} = event;
-
-    if (payload.action !== 'requested' && payload.action !== 'rerequested') {
-      return;
-    }
-
-    const data = {
-      owner: payload.repository.owner.login,
-      repo: payload.repository.name,
-      head_branch: payload.check_suite.head_branch,
-      head_sha: payload.check_suite.head_sha,
-    };
-
-    // Do the work async.
-    logger.info({data}, 'scheduling check creation');
-    setTimeout(() => {
-      const octokit = getOctokitFor(payload);
-      checks.create(octokit, data, logger);
-    });
-  });
-
-  webhooks.on([
-    'commit_comment',
-    'issue_comment',
-    'pull_request_review_comment',
-  ], async (event) => {
-    logger.debug({event}, 'comment event');
-
-    const {payload} = event;
-
-    if (payload.action !== 'created' && payload.action !== 'edited') {
-      return;
-    }
-
-    // To avoid a comment loop in the event of a mishap, do not react to any bot
-    // comments, importantly including our own ones.
-    if (payload.comment.user.type === 'Bot') {
-      logger.info(`ignoring bot comment from ${payload.comment.user.login}`);
-      return;
-    }
-
-    // Do nothing if this is not directed at @spec-test-bot.
-    // TODO: Avoid reacting to '@spec-test-botanist'
-    const atMentioned = payload.comment.body.includes('@spec-test-bot');
-    if (!atMentioned) {
-      return;
-    }
-
-    if (event.name !== 'issue_comment') {
-      logger.warn('comment reactions not implemented');
-      return;
-    }
-
-    const octokit = getOctokitFor(payload);
-
-    await octokit.reactions.createForIssueComment({
-      owner: payload.repository.owner.login,
-      repo: payload.repository.name,
-      comment_id: payload.comment.id,
-      content: '+1',
-    });
-  });
-
-  webhooks.on('error', (error) => {
-    logger.warn(error, 'webhook error');
   });
 
   return app;

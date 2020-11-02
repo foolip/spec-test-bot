993c1572dcc7d9a358c9022dcabeaa72a907b762
Tab Atkins-Bittner <jackalmage@gmail.com>
2020-11-02 09:01:39 -0800

    semi

--- test/app.js
+++ test/app.js
@@ -40,7 +40,7 @@ suite('express app', () => {
   test('checks route', async ()=> {
     const res = await agent.get('/checks/tabatkins/csswg-drafts/12345678abcdef/');
     assert.equal(res.status, 200);
-    assert.deepEqual(res.body, {"owner":"tabatkins","repo":"csswg-drafts","sha":"12345678abcdef"})
+    assert.deepEqual(res.body, {"owner":"tabatkins","repo":"csswg-drafts","sha":"12345678abcdef"});
   })
 
   // These signature test are not done as unit tests, as it is very
